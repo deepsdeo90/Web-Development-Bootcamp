a51f9da1c5aab8cab0123a11c357f5b1ba1425df
deepsdeo90 <deepalideore123@gmail.com>
2018-01-08 11:43:27 -0800

    Javascript exercise basic

---
+++ Javascript/1_prompt/script.js
@@ -0,0 +1,5 @@
+	var firstName = prompt("What is your first name?");
+	var lastName = prompt("What is your last name?");
+	var age = prompt("What is your dog's age?");
+	console.log("Your full name is "+firstName+" "+lastName);
+	console.log("Your dog's age "+age);
---
+++ Javascript/2_age_calculator/script.js
@@ -0,0 +1,4 @@
+	var age = prompt("What is your  age?");
+	var days = age*365.25; //for leap year
+	alert("You leaved roughly "+days+ " days");
+	
